7440b345c99e7150dd1c67bcf5e24ff9a165bac5
aka <0909090>
2022-10-12 21:35:58 +0800

    three

--- untitled/src/main/java/com/node/Test.java
+++ untitled/src/main/java/com/node/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("idea集成git");
         System.out.println("测试修改代码提交");
+        System.out.println("测试提交时加上配置文件");
     }
 }
