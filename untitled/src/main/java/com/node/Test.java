d5ed97e7d0a72679b7b9c40198b3e60bed595ed3
aka <0909090>
2022-10-12 21:57:49 +0800

    master ct

--- untitled/src/main/java/com/node/Test.java
+++ untitled/src/main/java/com/node/Test.java
@@ -6,6 +6,6 @@ public class Test {
         System.out.println("测试修改代码提交");
         System.out.println("测试提交时加上配置文件");
         System.out.println("test");
-        System.out.println("hot-fix");
+        System.out.println("chongtu2");
     }
 }
